19afbf393ddcf438b12f8d72b9f88abd649a3e6b
Mauludatu <funkyfulani@gmail.com>
2018-10-24 23:12:46 +0100

    customized Highlights pages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,7 +13,7 @@ class ApplicationController < ActionController::Base
         options[:apiKey] = @@news_api_key
         mUri = URI("https://newsapi.org/v2/everything")
         newuri = URI::HTTP.build( host: mUri.host , path: mUri.path , query: options.to_query)
-        #url = 'https://newsapi.org/v2/top-headlines?country=us&apiKey=18aaacfb0dd14d53983514dd807bc2f7'
+        url = 'https://newsapi.org/v2/everything?country=us&apiKey=6058bea4bb6e4df897d7ad26cfe3dd71'
         response = HTTParty.get(newuri)
         all_headlines = response.parsed_response
         return all_headlines
@@ -25,18 +25,19 @@ class ApplicationController < ActionController::Base
 
 
   # method definition to get the top news for a country
-  def get_top_news(options)
-
-       options[:apiKey] = @@news_api_key
-       mUri = URI("https://newsapi.org/v2/top-headlines")
-       newuri = URI::HTTP.build( host: mUri.host , path: mUri.path , query: options.to_query)
-       url = 'https://newsapi.org/v2/top-headlines?country=uk&apiKey=18aaacfb0dd14d53983514dd807bc2f7'
-       if options
-       response = HTTParty.get(newuri)
-       else
-       response = HTTParty.get(url)
-       end
-       top_headlines = response.parsed_response
+  def get_top_news(options={})
+          if options == {}
+              # options = args[0]
+              options[:apiKey] = @@news_api_key
+              url = 'https://newsapi.org/v2/top-headlines?country=gb&apiKey=6058bea4bb6e4df897d7ad26cfe3dd71'
+              response = HTTParty.get(url)
+          else
+              options[:apiKey] = @@news_api_key
+              mUri = URI("https://newsapi.org/v2/top-headlines")
+              newuri = URI::HTTP.build( host: mUri.host , path: mUri.path , query: options.to_query)
+              response = HTTParty.get(newuri)
+            end
+            top_headlines = response.parsed_response
        return top_headlines
   end
 
--- app/controllers/news_controller.rb
+++ app/controllers/news_controller.rb
@@ -1,37 +1,37 @@
 class NewsController < ApplicationController
 
-  def index
+        def index
 
-          @top_headlines = get_all_news(q: 'Damilola')
+                # @top = get_all_news(q: "hauwa")
+                @top_headlines = get_top_news()
+        end
 
-  end
 
+        def search
+           @keyword = params[:q]
+           @results = get_all_news(q: @keyword)
+           @top = get_top_news(q: @keyword)
 
-      def search
-        @keyword = params[:q]
-        @results = get_all_news(q: @keyword)
-        @top_headlines = get_top_news(q: @keyword)
+          # query = params["q"]
+          # keyword_exist = Keyword.find_by(keyword: query.to_s)
+          #
+          # # @results = get_all_news(q: params["q"])
+          # # @top_headlines = get_top_news(q: params["q"])
+          #
+          # if keyword_exist
+          #     keyword_exist.hit_rate +=  1
+          #     keyword_exist.save
+          #     @usersearch = Usersearch.create!(q: query , status: @results.status , totalresults: @results.totalResults)
+          #
+          # else
+          #     new_keyword = Keyword.new(keyword: query , hit_rate: 0)
+          #     new_keyword.save
+          #     @usersearch = Usersearch.create!(q: query , status: @results.status , totalresults: @results.totalResults)
+          # end
 
-        # query = params["q"]
-        # keyword_exist = Keyword.find_by(keyword: query.to_s)
-        #
-        # # @results = get_all_news(q: params["q"])
-        # # @top_headlines = get_top_news(q: params["q"])
-        #
-        # if keyword_exist
-        #     keyword_exist.hit_rate +=  1
-        #     keyword_exist.save
-        #     @usersearch = Usersearch.create!(q: query , status: @results.status , totalresults: @results.totalResults)
-        #
-        # else
-        #     new_keyword = Keyword.new(keyword: query , hit_rate: 0)
-        #     new_keyword.save
-        #     @usersearch = Usersearch.create!(q: query , status: @results.status , totalresults: @results.totalResults)
-        # end
+          redirect_to usersearches_search_path
 
-        redirect_to usersearches_search_path
-
-      end
+        end
 
       def usersearch_params
           params.permit(:q)
