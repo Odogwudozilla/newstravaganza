65c85bf6122b9075e46e87e4eedd26f51ded5ff3
Chidozie Nnachor <odogwu.naija@gmail.com>
2018-10-27 11:45:36 +0100

    heroku db migrate error

--- db/migrate/20181025230639_change_usersearches_column.rb
+++ db/migrate/20181025230639_change_usersearches_column.rb
@@ -1,7 +1,7 @@
 class ChangeUsersearchesColumn < ActiveRecord::Migration[5.2]
   def change
-    remove_column :usersearches, :integer
-    remove_column :usersearches, :status
-    change_column :usersearches, :totalresults, :integer
+    # remove_column :usersearches, :integer
+    # remove_column :usersearches, :status
+    # change_column :usersearches, :totalresults, :integer
   end
 end
